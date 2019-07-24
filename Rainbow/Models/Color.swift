dbe6cfbdb433cffbdec42e23b4dfee7004f6c1ab
dillonp23 <dillonp.lambda@gmail.com>
2019-07-23 20:01:56 -0700

    Added colors in table view and ability to segue to a color detail view with colored background

---
+++ Rainbow/Models/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Dillon P on 7/23/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+// model typically named the singular form of your table view controller
+
+// import Foundation -- > can remove Foundation for this file
+import UIKit // need to import this UIColor framework into this file in order to access UIColor down below
+
+struct MyColor {
+    let name: String // show the name in table view
+    let color: UIColor // show the color in the detail view
+}
--- Rainbow/View Controllers/ColorsTableViewController.swift
+++ Rainbow/View Controllers/ColorsTableViewController.swift
@@ -10,81 +10,57 @@ import UIKit
 
 class ColorsTableViewController: UITableViewController {
 
+    var colors: [MyColor] = [MyColor(name: "Red", color: .red),
+                             MyColor(name: "Orange", color: .orange),
+                             MyColor(name: "Yellow", color: .yellow),
+                             MyColor(name: "Green", color: .green),
+                             MyColor(name: "Blue", color: .blue),
+                             MyColor(name: "Cyan", color: .cyan),
+                             MyColor(name: "Purple", color: .purple)]
+    
+    // this array of colors is referred to as our data source.. instead of hard coding the number of rows just use coiunt of the array
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // self.title = "Rainbow" -->> can either set here or set in Main.storyboard
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return colors.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        
+        let color = colors[indexPath.row] // pronounced "colors, sub, indexpath.row"
+        cell.textLabel?.text = color.name
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+   
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) { // sender would be what we tapped
+        // in this case a cell, but could also be a button
+        // this segue is called for every single segue
+        
+        if segue.identifier == "ShowColorSegue" {
+            
+            guard let indexPath = tableView.indexPathForSelectedRow,
+                let colorDetialVC = segue.destination as? ColorDetailViewController else { return }
+            
+            let cellColor = colors[indexPath.row]
+            
+            colorDetialVC.cellColor = cellColor
+        }
     }
-    */
-
 }
